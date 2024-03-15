5510788fde5bf389634e7fd1737c316e764a0e62
VarunKvK <varunkrishnankv2002@gmail.com>
2024-03-15 22:27:06 +0530

    Used Plotly this time

--- BeautifulCharts/main.py
+++ BeautifulCharts/main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import plotly.express as px
 
 app_df=pd.read_csv("apps.csv")
 app_df.head()
@@ -20,3 +21,28 @@ df.shape
 df.sort_values("Rating",ascending=False).head()
 df.sort_values("Size_MBs",ascending=False).head()
 df.sort_values("Reviews",ascending=False).head(50)
+
+ratings=df.Content_Rating.value_counts()
+ratings
+
+fig=px.pie(labels=ratings.index,values=ratings.values, title="Content Rating", names=ratings.index, hole=0.6)
+fig.update_traces(textposition="outside",textinfo="percent+label")
+fig
+
+df.head(2)
+df.Installs.value_counts()
+df.Installs.describe()
+df.info()
+
+df.Installs=df.Installs.astype(str).str.replace(",","")
+df.Installs=pd.to_numeric(df.Installs)
+df.Installs.describe()
+df[["App","Installs"]].groupby("Installs").count()
+
+
+df.head(1)
+df.Price=df.Price.astype(str).str.replace("$","")
+df.Price=pd.to_numeric(df.Price)
+df.Price.describe()
+df[["App","Price"]].groupby("Price").count().sort_values("Price",ascending=False).head(20)
+
