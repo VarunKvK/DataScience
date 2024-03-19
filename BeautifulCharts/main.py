653af8dac2a7540ce6142cc566c0358d5f9aeb05
VarunKvK <varunkrishnankv2002@gmail.com>
2024-03-19 18:03:17 +0530

    Created a bar plot using plotly and its actually getting pretty intense

--- BeautifulCharts/main.py
+++ BeautifulCharts/main.py
@@ -44,5 +44,51 @@ df.head(1)
 df.Price=df.Price.astype(str).str.replace("$","")
 df.Price=pd.to_numeric(df.Price)
 df.Price.describe()
-df[["App","Price"]].groupby("Price").count().sort_values("Price",ascending=False).head(20)
+df.sort_values("Price",ascending=False).head(20)
 
+df=df[df["Price"]<250]
+df.sort_values("Price",ascending=False).head(20)
+
+df["Revenue_Estimate"]=df.Installs.mul(df.Price)
+df.sort_values("Revenue_Estimate",ascending=False).head(10)
+
+# df.Category.nunique
+
+top_10=df.Category.value_counts().head(10)
+top_10
+bar_chart=px.bar(x=top_10.index,y=top_10.values)
+bar_chart.show()
+
+category_installs=df.groupby("Category").agg({"Installs":pd.Series.sum})
+category_installs.sort_values("Installs",ascending=False,inplace=True)
+category_installs.head(10)
+category_plot=px.bar(x=category_installs.Installs, y=category_installs.index,orientation='h',title="Category Installs")
+category_plot.update_layout(xaxis_title="Installs",yaxis_title="Category")
+
+number_installs=df.groupby("Category").agg({"App":pd.Series.count})
+number_installs.head(10)
+category_apps=pd.merge(number_installs,category_installs,on="Category",how="inner")
+category_apps.head(10)
+category_apps.sort_values("App",ascending=True,inplace=True)
+scatter=px.scatter(category_apps,x="App",y="Installs",title="Category based on app downlads",size="Installs",hover_name=category_apps.index,color="Installs")
+scatter.show()
+
+df.groupby("Genres").describe()
+len(df.groupby("Genres").nunique())
+df.Genres.value_counts().sort_values(ascending=True)[:5]
+stack=df.Genres.str.split(";",expand=True).stack()
+num_genres=stack.value_counts()
+num_genres
+
+num_genre_bar= px.bar(x=num_genres.index[:15],y=num_genres.values[:15],orientation='v',title="Top Genres",hover_name=num_genres.values[:15],color=num_genres.values[:15],color_continuous_scale='tealrose')
+num_genre_bar.update_layout(xaxis_title="Genres",yaxis_title="Number of Apps")
+num_genre_bar.show()
+
+#df.Types
+df.Type.value_counts()
+df_free_vs_paid=df.groupby(['Category',"Type"],as_index=False).agg({'App':pd.Series.count})
+df_free_vs_paid
+
+free_vs_paid_chart=px.bar(df_free_vs_paid[:15],x="Category",y="App",title="Free vs Paid", barmode='group',color="Type")
+free_vs_paid_chart.update_layout(xaxis_title="App", yaxis_title="Category")
+free_vs_paid_chart
