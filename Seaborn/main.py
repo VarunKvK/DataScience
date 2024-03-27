7ef4bcddb7813cc7a3dda226b3e8c4871ee90f59
VarunKvK <varunkrishnankv2002@gmail.com>
2024-03-27 13:57:06 +0530

    Learnt about querying in Pandas

--- Seaborn/main.py
+++ Seaborn/main.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 data=pd.read_csv("./cost_revenue_dirty.csv")
 data.shape
+len(data)
 data.isna().values.any()
 data.duplicated().values.any()
 data.info()
@@ -31,3 +32,21 @@ data[data.USD_Production_Budget==425000000]
 
 zero_income=data[data.USD_Worldwide_Gross==0]
 zero_income.sort_values("USD_Production_Budget",ascending=False)
+
+international_revenue=data[(data.USD_Worldwide_Gross != 0) & (data.USD_Domestic_Gross == 0)]
+international_revenue
+
+df_filter=data.query('USD_Worldwide_Gross !=0 and USD_Domestic_Gross==0')
+len(df_filter)
+
+time_stamp=pd.Timestamp('2018-05-01')
+unreleased_movies=data[data.Release_Date >=time_stamp]
+unreleased_movies.sort_values('Release_Date',ascending=False)
+len(unreleased_movies)
+
+data_clean=data.drop(unreleased_movies.index)
+len(data_clean)
+data_clean.describe()
+
+money_loss=data_clean[data_clean.USD_Production_Budget > data_clean.USD_Worldwide_Gross]
+(len(money_loss)/len(data_clean))*100
